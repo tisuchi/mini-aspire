f417c0813aaec9bb61df87acd34bfe562cfaaf7d
Thouhedul Islam <tisuchi@gmail.com>
2018-11-21 15:16:56 +0600

    Show a particular user.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers;
 
 use Response;
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+	/**
+	 * Return list of the users
+	 * @return [type] [description]
+	 */
     public function index()
     {
     	return Response::json([	
 			'status' => 'success',
             'users' => User::latest()->paginate(10)
+		], 200); 
+    }
+
+    public function show(User $user)
+    {
+    	return Response::json([	
+			'status' => 'success',
+            'user' => $user
 		], 200); 
     }
 }
