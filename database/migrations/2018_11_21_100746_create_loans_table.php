9095743943f6f233ca4e31968742fcab40c5930e
Thouhedul Islam <tisuchi@gmail.com>
2018-11-21 16:11:52 +0600

    Create a migration for loans

---
+++ database/migrations/2018_11_21_100746_create_loans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('months');
+            $table->string('original_amount');
+            $table->string('extra_amount_monthly');
+            $table->string('total_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+}
